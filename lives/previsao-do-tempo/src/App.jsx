58b6ef3761a12ba7ce00d29f4761cdfab38ed217
Victor Pantoja <victor.pko@outlook.com>
2021-07-20 14:27:12 -0300

    live-weather-03
    Input de pesquisa criado.

--- lives/previsao-do-tempo/src/App.jsx
+++ lives/previsao-do-tempo/src/App.jsx
@@ -5,22 +5,33 @@ import { api } from './services/api-time';
 function App() {
 
   const  [weather, setWeather] = useState(null)
-  const city = 'Americana'
+  const [city, setCity] = useState('')
+  const [search, setSearch] = useState('')
 
-  async function handleGetWeather() {
-    const response = await api.get(city)
+  async function handleGetWeather(event) {
+    event.preventDefault()
+    const response = await api.get(search)
+    setCity(search)
+
     console.log(response.data)
     setWeather(response.data)
 
   }
 
   useEffect(() => {
-    handleGetWeather()
-  })
+    //handleGetWeather()
+  }, [])
   
   return (
     <div id="app">
       <div id="container">
+
+        <form onSubmit={handleGetWeather}>
+          <input
+            type="text" value={search} onChange={(event) => setSearch(event.target. value)} 
+          />
+          <button onClick={handleGetWeather}>Procurar</button>
+        </form>
 
         {weather &&
           <main>
